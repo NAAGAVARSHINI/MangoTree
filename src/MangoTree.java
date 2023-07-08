0bda216c8151de690a98ad15ab226badc07375ef
NAAGAVARSHINI <naagavarshinim@gmail.com>
2023-07-08 19:52:32 +0530

    initial commit

--- src/MangoTree.java
+++ src/MangoTree.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MangoTree {
+import java.util.Scanner;
+
+public class MangoTree {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int row= sc.nextInt();
+        int col=sc.nextInt();
+        int tree= sc.nextInt();
+        if(tree >=1 && tree<=row || tree%col==0 || tree%col==1){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
 }
